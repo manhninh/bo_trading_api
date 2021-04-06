0d843a31cca3c7c4f0d2807445438e9f357e9899
ninh.pham <manhninh91@gmail.com>
2021-04-06 10:30:54 +0700

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
-const envFound = config({path: `./.env.${process.env.NODE_ENV || 'development'}`});
+const envFound = config({ path: `./.env.${process.env.NODE_ENV || 'development'}` });
 if (!envFound) throw new Error("Couldn't find .env file");
 
 export default {
@@ -8,11 +8,9 @@ export default {
 
   port: process.env.PORT || 5000,
 
-  logs: {level: process.env.LOG_LEVEL || 'silly'},
+  logs: { level: process.env.LOG_LEVEL || 'silly' },
 
   MONGODB_URI: process.env.MONGODB_URI,
-
-  BINANCE_BASE_ENDPOINT: process.env.BINANCE_BASE_ENDPOINT,
 
   TOKEN_LIFE: process.env.TOKEN_LIFE,
 
