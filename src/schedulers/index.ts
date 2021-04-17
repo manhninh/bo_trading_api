23e3cb1cd864a00ff8939eb7462f9076d4f4cada
manhnguyenpg <manh.nguyentuan@powergatesoftware.com>
2021-04-17 09:45:13 +0700

    Cronjob to verify withdraw TX

--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -1,10 +1,12 @@
 import ImportSystemTRC20Deposits from '@src/controllers/wallet/ImportSystemTRC20Deposits';
 import ImportTRC20Deposits from '@src/controllers/wallet/ImportTRC20Deposits';
+import ScheduleVerifyTX from '@src/controllers/wallet/ScheduleVerifyTX';
 import scheduler from 'node-schedule';
 export default class Scheduler {
   public config() {
     // scheduler.scheduleJob(config.SCHEDULE_EXECUTE_EVERY_MINUTE, executeEveryMinute);
     scheduler.scheduleJob('*/5 * * * *', ImportTRC20Deposits);
     scheduler.scheduleJob('*/5 * * * *', ImportSystemTRC20Deposits);
+    scheduler.scheduleJob('*/5 * * * *', ScheduleVerifyTX);
   }
 }
