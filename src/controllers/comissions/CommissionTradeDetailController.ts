3339b5b5995ec785de470797551a9fdda8801a3d
Ninh Phạm <manhninh91@gmail.com>
2021-04-26 14:52:37 +0700

    change request body to query

--- src/controllers/comissions/CommissionTradeDetailController.ts
+++ src/controllers/comissions/CommissionTradeDetailController.ts
@@ -9,11 +9,11 @@ export const CommissionTradeDetailController = async (
 ): Promise<void> => {
   try {
     const userId = req.user['id'];
-    const params = req.body;
+    const params = req.query;
     const data = new CommissionTradeDetailValidator();
     data.userId = userId;
-    data.fromDate = new Date(params.fromDate);
-    data.toDate = new Date(params.toDate);
+    data.fromDate = new Date(params.fromDate.toString());
+    data.toDate = new Date(params.toDate.toString());
     data.page = Number(params.page);
     data.limit = Number(params.limit);
     const result = await CommissionTradeDetailBusiness(data);
