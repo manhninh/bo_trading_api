4b8dd2c88d6a4c0f27f2f447a00939a0b31292dc
manhnguyenpg <manh.nguyentuan@powergatesoftware.com>
2021-06-22 23:55:52 +0700

    Fix for loop async

--- src/business/wallet/ImportERC20DepositsBusiness.ts
+++ src/business/wallet/ImportERC20DepositsBusiness.ts
@@ -17,118 +17,125 @@ export const importERC20Deposits = async (): Promise<any> => {
     const configModel = new SystemConfigRepository();
     const configERC20Deposit = await configModel.findOne({ key: config.SYSTEM_ERC20_AUTO_TRANSFER_TO_COOL_WALLET });
     const configERC20DepositValue = await configModel.findOne({ key: config.SYSTEM_ERC20_AUTO_TRANSFER_TO_COOL_WALLET_MIN_AMOUNT });
-    const walletModel = new UserWalletRepository();
-    const txModel = new UserTransactionsRepository();
+
 
     if (rows !== undefined && rows.length) {
       // First contstruct a tronWeb object with a private key
-      rows.forEach(async row => {
-        try {
-          // START: Check for ERC20
-          if (row?.erc20) {
-            const ethWallet = JSON.parse(row.erc20);
-            if (ethWallet !== undefined && typeof ethWallet == 'object') {
-              const walletAddress = ethWallet.address;
-
-              // Check amount user wallet and withdraw to cold wallet
-              try {
-                const userETHWalletBalance = await getBalanceEth(ethWallet.address_private_key, walletAddress);
-                const userUSDTWalletBalance = await getBalanceUsdt(ethWallet.address_private_key, walletAddress);
-                // Send fee for withdraw usdt
-                if (userUSDTWalletBalance >= wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT || userUSDTWalletBalance >= configERC20DepositValue?.value) {
-                  // Add to wallet balance (temp) => amount_trc20_wallet
-                  const currentAmountWallet = Number(row?.amount_erc20_wallet) || 0;
-                  const realBalance = Number(userUSDTWalletBalance) - currentAmountWallet;
-                  if (realBalance >= Number(wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT)) {
-                    walletModel.updateByUserId(row.user_id, { $inc: { amount: realBalance } });
-                  }
-                  walletModel.updateByUserId(row.user_id, { amount_erc20_wallet: Number(userUSDTWalletBalance) });
-
-                  let userTx;
-                  if (realBalance >= Number(wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT)) {
-                    // Create User transaction
-                    userTx = await createDepositTransaction(row, realBalance, config.ETH_ERC20_SYMBOL, walletAddress, null);
-                  } else {
-                    userTx = true;
-                  }
-                  if (userTx) {
-                    if (configERC20Deposit && Boolean(configERC20Deposit.value) && userUSDTWalletBalance >= configERC20DepositValue?.value) {
-                      // Send fee to User address if we need to transfer USDT from User to COOL WALLET
-                      if (Number(userETHWalletBalance) < 0.01) {
-                        const hotETHWalletBalance = await getBalanceEth(wallet.HOT_WALLET_ETH_PRIVATE_KEY, wallet.HOT_WALLET_ETH_ADDRESS);
-                        if (Number(hotETHWalletBalance) > 0.01) {
-                          const tx = await sendEth(
-                            wallet.HOT_WALLET_ETH_PRIVATE_KEY,
-                            wallet.HOT_WALLET_ETH_ADDRESS,
-                            walletAddress,
-                            '0.01'
-                          );
-
-                          if (tx) {
-                            await createSystemTransaction(row, 0.01, wallet.ETH_ERC20_SYMBOL, walletAddress, tx);
-                          }
-                        }
-                      } else {
-                        // Get config gas from DB - Admin setup
-                        const configWithdrawGasPrice = await configModel.findOne({ key: config.SYSTEM_ERC20_GAS_KEY });
-                        try {
-                          const tx = await sendUsdt(
-                            ethWallet.address_private_key,
-                            walletAddress,
-                            wallet.COOL_WALLET_ETH_ADDRESS,
-                            String(userUSDTWalletBalance),
-                            (configWithdrawGasPrice ? configWithdrawGasPrice.value : '90')
-                          );
-                          if (tx) {
-                            await txModel.updateMany(
-                              {
-                                user_id: row.user_id,
-                                symbol: config.ETH_ERC20_SYMBOL,
-                                system_status: Constants.TRANSACTION_STATUS_PROCESSING,
-                                type: Constants.TRANSACTION_TYPE_DEPOSIT,
-                                address: walletAddress,
-                                tx: null
-                              },
-                              {
-                                tx: String(tx),
-                                system_status: Constants.TRANSACTION_STATUS_PENDING
-                              }
-                            );
-                          }
-                        } catch (error) {
-                          console.log('ERROR DEPOSIT ERC20', error);
-                        }
-                      }
-                    }
-                  }
-                }
-              } catch (werr) {
-                throw new Error('ERC20: ' + werr);
-              }
-              /*if (Number(userETHWalletBalance) > 0.01) {
-                const txHash = await sendEth(
-                  ethWallet.address_private_key,
-                  walletAddress,
-                  walletConstant.COOL_WALLET_ETH_ADDRESS,
-                  (Number(userETHWalletBalance) - 0.01) as Number
-                );
-              }*/
-            }
-          } else {
-            // const user = {
-            //   id: row._id
-            // };
-            // createUSDTERC20(user);
-          }
-          // END: Check for ERC20
-
-          await delay(1000);
-        } catch (err) {
-          await delay(1000);
-        }
-      });
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        await getUserERCBalance(row, configERC20Deposit, configERC20DepositValue);
+      }
     }
   } catch (err) {
     throw err;
   }
+};
+
+const getUserERCBalance = async (row, configERC20Deposit, configERC20DepositValue) => {
+  await new Promise(async (resolve) => {
+    try {
+      const walletModel = new UserWalletRepository();
+      const txModel = new UserTransactionsRepository();
+      const configModel = new SystemConfigRepository();
+
+      // START: Check for ERC20
+      if (row?.erc20) {
+        const ethWallet = JSON.parse(row.erc20);
+        if (ethWallet !== undefined && typeof ethWallet == 'object') {
+          const walletAddress = ethWallet.address;
+
+          // Check amount user wallet and withdraw to cold wallet
+          try {
+            const userETHWalletBalance = await getBalanceEth(ethWallet.address_private_key, walletAddress);
+            const userUSDTWalletBalance = await getBalanceUsdt(ethWallet.address_private_key, walletAddress);
+
+            console.log('ADDRESS-ERC: ', walletAddress, ' => ', userUSDTWalletBalance);
+
+            // Send fee for withdraw usdt
+            if (userUSDTWalletBalance >= wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT || userUSDTWalletBalance >= configERC20DepositValue?.value) {
+              // Add to wallet balance (temp) => amount_trc20_wallet
+              const currentAmountWallet = Number(row?.amount_erc20_wallet) || 0;
+              const realBalance = Number(userUSDTWalletBalance) - currentAmountWallet;
+              if (realBalance >= Number(wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT)) {
+                await walletModel.updateByUserId(row.user_id, { $inc: { amount: realBalance } });
+              }
+              await walletModel.updateByUserId(row.user_id, { amount_erc20_wallet: Number(userUSDTWalletBalance) });
+
+              let userTx;
+              if (realBalance >= Number(wallet.ETH_ERC20_DEPOSIT_MIN_AMOUNT)) {
+                // Create User transaction
+                userTx = await createDepositTransaction(row, realBalance, config.ETH_ERC20_SYMBOL, walletAddress, null);
+              } else {
+                userTx = true;
+              }
+              if (userTx) {
+                if (configERC20Deposit && Boolean(configERC20Deposit.value) && userUSDTWalletBalance >= configERC20DepositValue?.value) {
+                  // Send fee to User address if we need to transfer USDT from User to COOL WALLET
+                  if (Number(userETHWalletBalance) < 0.01) {
+                    const hotETHWalletBalance = await getBalanceEth(wallet.HOT_WALLET_ETH_PRIVATE_KEY, wallet.HOT_WALLET_ETH_ADDRESS);
+                    if (Number(hotETHWalletBalance) > 0.01) {
+                      const tx = await sendEth(
+                        wallet.HOT_WALLET_ETH_PRIVATE_KEY,
+                        wallet.HOT_WALLET_ETH_ADDRESS,
+                        walletAddress,
+                        '0.01'
+                      );
+
+                      if (tx) {
+                        await createSystemTransaction(row, 0.01, wallet.ETH_ERC20_SYMBOL, walletAddress, tx);
+                      }
+                    }
+                  } else {
+                    // Get config gas from DB - Admin setup
+                    const configWithdrawGasPrice = await configModel.findOne({ key: config.SYSTEM_ERC20_GAS_KEY });
+                    try {
+                      const tx = await sendUsdt(
+                        ethWallet.address_private_key,
+                        walletAddress,
+                        wallet.COOL_WALLET_ETH_ADDRESS,
+                        String(userUSDTWalletBalance),
+                        (configWithdrawGasPrice ? configWithdrawGasPrice.value : '90')
+                      );
+                      if (tx) {
+                        await txModel.updateMany(
+                          {
+                            user_id: row.user_id,
+                            symbol: config.ETH_ERC20_SYMBOL,
+                            system_status: Constants.TRANSACTION_STATUS_PROCESSING,
+                            type: Constants.TRANSACTION_TYPE_DEPOSIT,
+                            address: walletAddress,
+                            tx: null
+                          },
+                          {
+                            tx: String(tx),
+                            system_status: Constants.TRANSACTION_STATUS_PENDING
+                          }
+                        );
+                      }
+                    } catch (error) {
+                      console.log('ERROR DEPOSIT ERC20', error);
+                    }
+                  }
+                }
+              }
+            }
+          } catch (werr) {
+            throw new Error('ERC20: ' + werr);
+          }
+        }
+      } else {
+        /* const user = {
+          id: row._id
+        };
+        createUSDTERC20(user);*/
+      }
+      // END: Check for ERC20
+
+      await delay(1000);
+      return resolve(true);
+    } catch (err) {
+      await delay(1000);
+      return resolve(true);
+    }
+  });
 };
--- src/business/wallet/ImportERC20DepositsSystemBusiness.ts
+++ src/business/wallet/ImportERC20DepositsSystemBusiness.ts
@@ -10,58 +10,68 @@ import { getETHTransaction } from '../user/CreateWalletBusiness';
 export const importERC20DepositsSystem = async (): Promise<any> => {
   try {
     const transaction = new UserTransactionsRepository();
-    const walletModel = new UserWalletRepository();
     const rows = await transaction.getAllPendingTransactions(Constants.TRANSACTION_TYPE_DEPOSIT);
     if (rows !== undefined && rows.length) {
-      rows.forEach(async row => {
-        try {
-          // START: Check system_status for each TX
+      // Get all email from admin
+      const adminRepos = new AdminRepository();
+      const admins = await adminRepos.findAll();
+      const emailConfig = new EmailConfig(configSendEmail);
 
-          // Get all email from admin
-          const adminRepos = new AdminRepository();
-          const admins = await adminRepos.findAll();
-          const emailConfig = new EmailConfig(configSendEmail);
-
-          // FOR ERC20 - USDT
-          if (row.symbol == config.ETH_ERC20_SYMBOL && row?.tx) {
-            const result = await getETHTransaction(row.tx);
-            if (result) {
-              if (result?.status) {
-                // Cap nhat TX
-                transaction.updateById(row._id, { system_status: Constants.TRANSACTION_STATUS_SUCCESS });
-                // Cap nhat value trong temp wallet
-                walletModel.updateByUserId(row.user_id, { amount_erc20_wallet: 0 });
-
-              } else {
-                // Cap nhat TX
-                transaction.updateById(row._id, { system_status: Constants.TRANSACTION_STATUS_CANCELLED });
-
-                // Send email to admin
-                admins.map(async (admin) => {
-                  emailConfig.readHTMLFile(`${config.PATH_TEMPLATE_EMAIL}/transaction_error.html`, (html: string) => {
-                    const template = handlebars.compile(html);
-                    const replacements = {
-                      'username': admin.email,
-                      'link': config.TRON_EXPLORER + '#/transaction/' + row.tx
-                    };
-                    const htmlToSend = template(replacements);
-                    emailConfig
-                      .send(config.EMAIL_ROOT, admin.email, 'ERC20 - Transaction Error: Hot wallet not send enough ETH!', htmlToSend)
-                      .catch((err) => logger.error(err.message));
-                  });
-                });
-              }
-            }
-          }
-
-          // END: Check system_status for each TX
-          await delay(500);
-        } catch (err) {
-          await delay(0);
-        }
-      });
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        await SystemERCDeposit(row, admins, emailConfig);
+      }
     }
   } catch (err) {
     throw err;
   }
+};
+
+const SystemERCDeposit = async (row, admins, emailConfig) => {
+  await new Promise(async (resolve) => {
+    try {
+      // START: Check system_status for each TX
+      const transaction = new UserTransactionsRepository();
+      const walletModel = new UserWalletRepository();
+
+      // FOR ERC20 - USDT
+      if (row.symbol == config.ETH_ERC20_SYMBOL && row?.tx) {
+        const result = await getETHTransaction(row.tx);
+        if (result) {
+          if (result?.status) {
+            // Cap nhat TX
+            await transaction.updateById(row._id, { system_status: Constants.TRANSACTION_STATUS_SUCCESS });
+            // Cap nhat value trong temp wallet
+            await walletModel.updateByUserId(row.user_id, { amount_erc20_wallet: 0 });
+
+          } else {
+            // Cap nhat TX
+            await transaction.updateById(row._id, { system_status: Constants.TRANSACTION_STATUS_CANCELLED });
+
+            // Send email to admin
+            admins.map(async (admin) => {
+              emailConfig.readHTMLFile(`${config.PATH_TEMPLATE_EMAIL}/transaction_error.html`, (html: string) => {
+                const template = handlebars.compile(html);
+                const replacements = {
+                  'username': admin.email,
+                  'link': config.TRON_EXPLORER + '#/transaction/' + row.tx
+                };
+                const htmlToSend = template(replacements);
+                emailConfig
+                  .send(config.EMAIL_ROOT, admin.email, 'ERC20 - Transaction Error: Hot wallet not send enough ETH!', htmlToSend)
+                  .catch((err) => logger.error(err.message));
+              });
+            });
+          }
+        }
+      }
+
+      // END: Check system_status for each TX
+      await delay(1000);
+      return resolve(true);
+    } catch (err) {
+      await delay(1000);
+      return resolve(true);
+    }
+  });
 };
