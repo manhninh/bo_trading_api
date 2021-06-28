7ee9acdf195e7a54ab213eba99d6e2860a65b171
Ninh Phạm <manhninh91@gmail.com>
2021-06-28 12:48:35 +0700

    update code

--- src/routes/v1/admins/index.ts
+++ src/routes/v1/admins/index.ts
@@ -48,6 +48,7 @@ export default class UserRouters {
     SystemConfig(this.router);
     TotalTransaction(this.router);
     CommissionTradeUser(this.router);
+    GetTradeHistoryUser(this.router);
     GetTranferUserOnAdmin(this.router);
   }
 }
